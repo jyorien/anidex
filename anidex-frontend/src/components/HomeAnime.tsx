1843f99ae364e892a96ce16af1f77583e8d504f3
Joey Lim <jyorin0206@gmail.com>
2023-04-26 22:16:35 +0800

    centred title and made height of HomeAnime card fixed

--- anidex-frontend/src/components/HomeAnime.tsx
+++ anidex-frontend/src/components/HomeAnime.tsx
@@ -1,4 +1,12 @@
-import { Grid, Image, Stack, Text, createStyles } from "@mantine/core";
+import {
+  Center,
+  Container,
+  Grid,
+  Image,
+  Stack,
+  Text,
+  createStyles,
+} from "@mantine/core";
 
 const useStyles = createStyles((theme) => ({
   detailText: {
@@ -11,22 +19,32 @@ export function HomeAnime({ mal_id, name, type, status, src }: HomeAnimeData) {
   return (
     <Grid
       bg="#6C8496"
-      style={{ borderRadius: "3%", boxShadow: "2px 2px 2px #394147" }}
+      style={{
+        borderRadius: "3%",
+        boxShadow: "2px 2px 2px #394147",
+        height: "250px",
+      }}
     >
+      {/* image column */}
       <Grid.Col p="0" span={4}>
-        <Image src={src} radius="3% 0% 0% 3%" />
+        <Image src={src} radius="3% 0% 0% 3%" height="250px" />
       </Grid.Col>
+
+      {/* title, type, status column */}
       <Grid.Col span={8}>
         <Stack align="flex-start" justify="center" style={{ height: "100%" }}>
-          <Text
-            fz="32px"
-            fw="500"
-            className={classes.detailText}
-            lineClamp={2}
-            align="left"
-          >
-            {name}
-          </Text>
+          <Center>
+            <Text
+              fz="32px"
+              fw="500"
+              className={classes.detailText}
+              lineClamp={2}
+              align="left"
+            >
+              {name}
+            </Text>
+          </Center>
+
           <Text
             span={true}
             px="20px"
