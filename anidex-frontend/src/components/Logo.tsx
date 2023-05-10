575133d88523e5f1b21eefb5314bd8b0ae8630b0
Joey Lim <jyorin0206@gmail.com>
2023-05-10 15:58:38 +0800

    got rid of underline on logo

--- anidex-frontend/src/components/Logo.tsx
+++ anidex-frontend/src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export function AnidexLogo() {
   return (
-    <Link to="/">
+    <Link to="/" style={{ textDecoration: "none" }}>
       <Text fz="xl" c="white">
         Anidex
       </Text>
